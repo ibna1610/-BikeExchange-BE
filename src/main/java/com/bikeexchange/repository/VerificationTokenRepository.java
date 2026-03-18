a168f3402acc8c600b6d12670a1dd920233254aa
TruongTLT2 <truongtltse173154@fpt.edu.vn>
2026-03-18 23:35:27 +0700

    verify

--- src/main/java/com/bikeexchange/repository/VerificationTokenRepository.java
+++ src/main/java/com/bikeexchange/repository/VerificationTokenRepository.java
@@ -2,7 +2,10 @@ package com.bikeexchange.repository;
 
 import com.bikeexchange.model.User;
 import com.bikeexchange.model.VerificationToken;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,4 +15,8 @@ public interface VerificationTokenRepository extends JpaRepository<VerificationT
     Optional<VerificationToken> findByTokenAndType(String token, VerificationToken.TokenType type);
     Optional<VerificationToken> findByUserAndType(User user, VerificationToken.TokenType type);
     void deleteByUserAndType(User user, VerificationToken.TokenType type);
+
+    @Modifying
+    @Query("delete from VerificationToken vt where vt.id = :id")
+    int deleteByIdSafe(@Param("id") Long id);
 }
